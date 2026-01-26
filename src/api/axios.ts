59b9cf419388c67511eda442a2da6a2b84dd3fff
masud2005 <masud20012005@gmail.com>
2026-01-27 00:09:41 +0600

    project structure setup

---
+++ src/api/axios.ts
@@ -0,0 +1,14 @@
+
+
+import { createApi } from '@reduxjs/toolkit/query/react'
+import axiosBaseQuery from './axiosBaseQuery'
+export const baseApi = createApi({
+    reducerPath: "baseApi",
+    //    baseQuery: fetchBaseQuery({
+    //   baseUrl: config.baseUrl,
+    //   credentials: "include",
+    // }),
+    baseQuery: axiosBaseQuery(),
+    tagTypes: ["USER", "TOUR"],
+    endpoints: () => ({})
+})
